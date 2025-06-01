9085adfdcd16d70f49b1b54137f282a9d3060abb
bitzentrum <benjamin.gangl@bitzentrum.com>
2025-06-01 22:49:06 +0200

    Fixup implementing markdown module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
+import { importProvidersFrom } from '@angular/core';
+import { MarkdownModule } from 'ngx-markdown';
+import { provideHttpClient } from '@angular/common/http';
+
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(MarkdownModule.forRoot())
+  ],
 });
